96464de43365b5acf7d0e4f4da8d28932ea4c813
hwasuk528 <hwasuk528@naver.com>
2023-10-13 16:10:49 +0900

    hhghg

---
+++ hwado/src/main/java/com/example/hwado/HwadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwadoApplication.class, args);
+	}
+
+}
---
+++ hwado/src/test/java/com/example/hwado/HwadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
