6030e589910e82137f4a0429c6eb4c22df4b3d28
hwasuk528 <hwasuk528@naver.com>
2023-10-24 17:37:03 +0900

    [feat] 카카오로그인 기능 추가

--- ssafit/src/main/java/com/ssafy/ssafit/controller/UserController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.ssafy.ssafit.controller;
-
+import org.springframework.http.HttpHeaders;
 import com.ssafy.ssafit.service.userService.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +34,72 @@ public class UserController {
     회원수정
     회원조회
      */
+    @Autowired
+    UserService ks;
+
+
+
+    @GetMapping("/do")
+    public String loginPage()
+    {
+        return "kakao/login";
+    }
+
+    @GetMapping("/kakao")
+    public String getCI(@RequestParam String code, Model model) throws IOException {
+        System.out.println("code = " + code);
+        String access_token = ks.getToken(code);
+        Map<String, Object> userInfo = ks.getUserInfo(access_token);
+        model.addAttribute("code", code);
+        model.addAttribute("access_token", access_token);
+        model.addAttribute("userInfo", userInfo);
+
+        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
+        return "index";
+    }
+
+    @GetMapping("/auth/joinForm") // 회원가입하는데 인증필요없으므로 /auth
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/auth/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/member/kakao")
+    public @ResponseBody String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
+
+        // POST 방식으로 key=value 데이터를 요청 (카카오쪽으로)
+        // 이 때 필요한 라이브러리가 RestTemplate, 얘를 쓰면 http 요청을 편하게 할 수 있다.
+        RestTemplate rt = new RestTemplate();
+
+        // HTTP POST를 요청할 때 보내는 데이터(body)를 설명해주는 헤더도 만들어 같이 보내줘야 한다.
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // body 데이터를 담을 오브젝트인 MultiValueMap를 만들어보자
+        // body는 보통 key, value의 쌍으로 이루어지기 때문에 자바에서 제공해주는 MultiValueMap 타입을 사용한다.
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "492c50fd0e56f48e1008e9f9078acfdc");
+        params.add("redirect_uri", "http://localhost:8090/member/kakao/callback");
+        params.add("code", code);
+
+        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
+        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoTokenRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+        return "카카오 토큰 요청 완료 : 토큰 요청에 대한 응답 : "+response;
+    }
+
 
 }
--- ssafit/src/main/java/com/ssafy/ssafit/domain/Video.java
+++ ssafit/src/main/java/com/ssafy/ssafit/domain/Video.java
@@ -1,4 +1,25 @@
 package com.ssafy.ssafit.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Video {
+    @Id
+    private int videoNum;
+    @Column(nullable = false, length =50)
+    private String videoTitle;
+    @Column(nullable = false, length =300)
+    private String videoUrl;
+    @Column(nullable = false, length =20)
+    private String videoSort;
+
 }
