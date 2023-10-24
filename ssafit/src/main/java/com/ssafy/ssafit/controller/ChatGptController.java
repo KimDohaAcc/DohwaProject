3e25d59c2811db444bbcc81f6a856b542c3edb58
KimDohaAcc <8772158@naver.com>
2023-10-24 16:21:38 +0900

    [Feat] chatGPT 연동

---
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/ChatGptController.java
@@ -0,0 +1,18 @@
+package com.ssafy.ssafit.controller;
+
+import com.ssafy.ssafit.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatGptController {
+    private final ChatService chatService;
+    //chat-gpt 와 간단한 채팅 서비스 소스
+    @PostMapping("/gpt")
+    public String test(@RequestBody String question) {
+        return chatService.getChatResponse(question);
+    }
+}
