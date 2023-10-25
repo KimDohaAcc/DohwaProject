dc2d6ee0f072141b8529f476c4e511a2696057c2
hwasuk528 <hwasuk528@naver.com>
2023-10-25 11:41:17 +0900

    [feat] 카카오로그인 기능 추가

--- ssafit/src/main/java/com/ssafy/ssafit/service/userService/UserService.java
+++ ssafit/src/main/java/com/ssafy/ssafit/service/userService/UserService.java
@@ -4,7 +4,5 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface UserService {
-    String getToken(String code) throws IOException;
 
-    Map<String, Object> getUserInfo(String accessToken) throws IOException;
 }
