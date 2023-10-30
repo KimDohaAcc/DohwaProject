70cf52e607105576a1088ef1520227c2aa7b53bf
KimDohaAcc <8772158@naver.com>
2023-10-30 17:48:27 +0900

    [Feat] Like, Reserve 기능 구현 / json sample 추가

--- ssafit/src/main/java/com/ssafy/ssafit/controller/ReserveController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/ReserveController.java
@@ -1,4 +1,43 @@
 package com.ssafy.ssafit.controller;
 
+import com.ssafy.ssafit.domain.Like;
+import com.ssafy.ssafit.domain.Reserve;
+import com.ssafy.ssafit.domain.User;
+import com.ssafy.ssafit.service.reserveService.ReserveService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
 public class ReserveController {
+    private final ReserveService reserveService;
+
+    @PostMapping("/reserve/create")
+    public void createReserve(@RequestParam Reserve reserve) {
+        reserveService.createReserve(reserve);
+    }
+
+    @PutMapping("/reserve/update")
+    public void updateReserve(@RequestParam Reserve reserve) {
+        reserveService.modifyReserve(reserve);
+    }
+
+    @DeleteMapping("/reserve/delete")
+    public void deleteReserve(@RequestParam Reserve reserve) {
+        reserveService.deleteReserve(reserve);
+    }
+
+    @GetMapping("/reserve/get")
+    public ResponseEntity<List<Reserve>> getReserveByUser(HttpSession session) {
+        Optional<List<Reserve>> reserveList = Optional.ofNullable((User) session.getAttribute("loginUser"))
+                .map(reserveService::getListByUser);
+        return reserveList.map(list -> new ResponseEntity<List<Reserve>>(list, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
 }
--- ssafit/src/main/java/com/ssafy/ssafit/repository/LikeRepository.java
+++ ssafit/src/main/java/com/ssafy/ssafit/repository/LikeRepository.java
@@ -1,4 +1,15 @@
 package com.ssafy.ssafit.repository;
 
-public interface LikeRepository {
+import com.ssafy.ssafit.domain.Like;
+import com.ssafy.ssafit.domain.User;
+import com.ssafy.ssafit.domain.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    Like findByUserAndVideo(User user, Video video);
+
+    List<Like> findAllByUser(User user);
 }
