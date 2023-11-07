a68af624a3f168e2618bf24fcc82c68e0effe0c2
hwasuk528 <hwasuk528@naver.com>
2023-11-07 16:10:39 +0900

    [feat] video 구현

--- ssafit/src/main/java/com/ssafy/ssafit/controller/VideoController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/VideoController.java
@@ -1,4 +1,51 @@
 package com.ssafy.ssafit.controller;
 
+import com.ssafy.ssafit.domain.Video;
+import com.ssafy.ssafit.service.videoService.VideoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin("*")
 public class VideoController {
+    private final VideoService videoService;
+
+    @GetMapping("/video/{id}")
+    public ResponseEntity<Video>detail(@PathVariable Long id){
+        return videoService.getVideo(id)
+                .map(video -> new ResponseEntity<>(video, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/video")
+    public ResponseEntity<Video> write(@RequestBody Video video){
+        System.out.println("video = " + video);
+        return Optional.ofNullable(videoService.writeVideo(video))
+                .map(createVideo -> new ResponseEntity<>(createVideo, HttpStatus.OK))
+                .orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/video/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(videoService.removeVideo(id)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/video/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Video updateVideo){
+        Video newVideo = videoService.modifyVideo(id,updateVideo);
+        if(newVideo != null){
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- ssafit/src/main/java/com/ssafy/ssafit/repository/BoardRepository.java
+++ ssafit/src/main/java/com/ssafy/ssafit/repository/BoardRepository.java
@@ -2,7 +2,11 @@ package com.ssafy.ssafit.repository;
 
 import com.ssafy.ssafit.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select c from Board c where id=:id")
+    Board asdf(@Param("id")Long id);
 }
