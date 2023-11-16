d48d15ca10e36c7ffa8c5b21fcc30b2de9f4f95c
hwasuk528 <hwasuk528@naver.com>
2023-11-16 17:57:35 +0900

    [feat] vue comment 생성

---
+++ vue/src/views/CommentView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <textarea v-model="comment"></textarea>
+      <button @click="submitComment">Submit</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        comment: "",
+      };
+    },
+    methods: {
+      submitComment() {
+        // Submit the comment to the server
+      },
+    },
+  };
+  </script>
