d31b0502d57278f6f875b8839bc4f2a958a537f0
hwasuk528 <hwasuk528@naver.com>
2023-11-17 17:36:44 +0900

    [feat] 댓글 삭제 기능

--- vue/vue-project/src/stores/comment.js
+++ vue/vue-project/src/stores/comment.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import axios from 'axios';
 import { useUserStore } from './user';
@@ -7,30 +7,42 @@ import { useBoardStore } from './board';
 const REST_COMMENT_API = 'http://localhost:8080/comment';
 
 export const useCommentStore = defineStore('comment', () => {
-  const comment = ref(''); // comment 변수를 ref로 선언합니다.
-  const comments = ref([]); 
+  const comment = ref('');
+  const comments = ref([]);
+
+  function deleteComment(id) {
+    comments.value = comments.value.filter((v) => v.num !== id);
+    console.log("댓글리스트 : "+comments);
+  }
 
   function submitComment(commentValue) {
     return new Promise((resolve, reject) => {
       console.log(commentValue);
       if (commentValue) {
-        axios.post(REST_COMMENT_API, commentValue )
+        axios.post(REST_COMMENT_API, commentValue)
           .then((response) => {
-            comments.value.push(response.data); 
-            resolve(); 
+            comments.value.push(response.data);
+            resolve();
           })
           .catch((error) => {
-            reject(error); // 실패 시 reject 호출
+            reject(error);
           });
       } else {
-        reject(new Error('댓글 내용을 입력하세요')); // 내용이 없으면 reject 호출
+        reject(new Error('댓글 내용을 입력하세요'));
       }
     });
   }
+
+  const filteredComments = computed(() => {
+
+    return comments.value;
+  });
 
   return {
     comment,
     comments,
     submitComment,
+    deleteComment,
+    filteredComments, // computed로 생성한 속성을 반환합니다.
   };
 });
