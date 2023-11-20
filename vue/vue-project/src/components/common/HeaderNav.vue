ea4de416f58d90d9a1ac904db911632db14cdee5
hwasuk528 <hwasuk528@naver.com>
2023-11-20 14:45:19 +0900

    [fix] 복구

--- vue/vue-project/src/components/common/HeaderNav.vue
+++ vue/vue-project/src/components/common/HeaderNav.vue
@@ -1,61 +1,61 @@
 <template>
-    <header>
-        <div>
-            <b-navbar toggleable="lg" type="light" variant="faded">
-                <b-navbar-brand href="#">
-                    <img src="@/assets/logo.jpg" class="d-inline-block align-top" alt="logo" width="50">
-                    <RouterLink to="/">
-                        healthPanda
-                    </RouterLink>
-                </b-navbar-brand>
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav>
-                        <b-nav-item href="#">
-                            <RouterLink to="/store">지점 소개</RouterLink>
-                        </b-nav-item>
-                        <b-nav-item href="#">
-                            <RouterLink to="/board">이용 후기</RouterLink>
-                        </b-nav-item>
-                        <b-nav-item href="#">
-                            <RouterLink to="/video">운동 추천 영상</RouterLink>
-                        </b-nav-item>
-                        <b-nav-item href="#">
-                            <RouterLink to="/reserve">전화상담예약</RouterLink>
-                        </b-nav-item>
-                    </b-navbar-nav>
-                    <b-navbar-nav v-if="!isLogin">
-                        <b-nav-item href="#">
-                            <RouterLink to="/login">
-                                로그인
-                            </RouterLink>
-                        </b-nav-item>
-                        <b-nav-item href="#">
-                            <RouterLink to="/regist">회원가입</RouterLink>
-                        </b-nav-item>
-                    </b-navbar-nav>
-                    <b-navbar-nav right v-else>
-                        <b-nav-item-dropdown>
-                            <template #button-content>
-                                <em>User</em>
-                            </template>
-                            <b-nav-item href="#">
-                                <RouterLink to="/myPage">
-                                    마이페이지
-                                </RouterLink>
-                            </b-nav-item>
-                            <b-nav-item href="#">
-                                <b-dropdown-item href="#" @click="kakaoLogout">
-                                    로그아웃
-                                </b-dropdown-item>
-                            </b-nav-item>
-                        </b-nav-item-dropdown>
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-navbar>
-        </div>
-    </header>
-    <main></main>
-    
+  <header>
+    <div>
+      <b-navbar toggleable="lg" type="light" variant="faded">
+        <b-navbar-brand href="#">
+          <img src="@/assets/logo.jpg" class="d-inline-block align-top" alt="logo" width="50">
+          <RouterLink to="/">
+            healthPanda
+          </RouterLink>
+        </b-navbar-brand>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">
+              <RouterLink to="/store">지점 소개</RouterLink>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <RouterLink to="/board">이용 후기</RouterLink>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <RouterLink to="/video">운동 추천 영상</RouterLink>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <RouterLink to="/reserve">전화상담예약</RouterLink>
+            </b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav v-if="!isLogin">
+            <b-nav-item href="#">
+              <RouterLink to="/login">
+                로그인
+              </RouterLink>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <RouterLink to="/regist">회원가입</RouterLink>
+            </b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav right v-else>
+            <b-nav-item-dropdown>
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-nav-item href="#">
+                <RouterLink to="/myPage">
+                  마이페이지
+                </RouterLink>
+              </b-nav-item>
+              <b-nav-item href="#">
+                <b-dropdown-item href="#" @click="kakaoLogout">
+                  로그아웃
+                </b-dropdown-item>
+              </b-nav-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </header>
+  <main></main>
+
 </template>
 
 <script setup>
@@ -66,7 +66,7 @@ const userStore = useUserStore();
 const isLogin = computed(() => !!userStore.loginUser);
 
 const kakaoLogout = function () {
-    userStore.logoutUser();
+  userStore.logoutUser();
 }
 
 </script>
--- vue/vue-project/src/components/user/MyPage.vue
+++ vue/vue-project/src/components/user/MyPage.vue
@@ -9,13 +9,6 @@
         <div><span class="info-name">카카오 연동</span><span>{{ userStore.loginUser.iskakao ? "사용 중" : "사용 안 함" }}</span></div>
         <div class="changable-info" v-if="!userStore.loginUser.iskakao"><span class="info-name">비밀번호</span><span>새 비밀번호 설정</span><i class="bi bi-arrow-right enter"></i></div>
       </div>
-    </div>
-  <div id="myPage">
-    <h1>{{ userStore.loginUser.nickname }}님의 마이페이지</h1>
-    <p>이메일: {{ userStore.loginUser.account }}</p>
-    <p>알림 설정 여부 : (추후 생성)</p>
-    <p>알림 시간 : </p>
-    <RouterLink :to="{ name: 'myMeal' }">내 식단 기록</RouterLink>
       <h3>예약 내역:</h3>
       <ul>
         <li v-for="(reservation, index) in reservations" :key="index">
@@ -25,17 +18,16 @@
           <button @click="deleteReservation(index)">삭제</button>
         </li>
       </ul>
-
+    </div>
   </div>
-
 </template>
 
 <script setup>
 import { useUserStore } from "@/stores/user";
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
-
 const userStore = useUserStore();
+
 const reservations = ref([]);
 console.log(reservations);
 const formatDate = (dateString) => {
@@ -46,38 +38,36 @@ const formatDate = (dateString) => {
 
 onMounted(() => {
 // 사용자의 예약 정보 가져오기
-let user = sessionStorage.getItem("loginUser"); // sessionStorage는 키, 밸류가 모두 문자열
-user = JSON.parse(user);
+  let user = sessionStorage.getItem("loginUser"); // sessionStorage는 키, 밸류가 모두 문자열
+  user = JSON.parse(user);
 
-const url = `http://localhost:8080/reserve/get/${user.id}`;
-console.log(url);
-console.log(user);
-axios.get(`http://localhost:8080/reserve/get/${user.id}`)
-  .then(response => {
-    reservations.value = response.data; // 백엔드에서 반환된 예약 목록 저장
-    console.log("정보를 가져왔습니다");
-    console.log(reservations.value);
-  })
-  .catch(error => {
-    console.error('예약 정보를 가져오는 중 에러 발생:', error);
-    console.log(reservations);
-  });
+  const url = `http://localhost:8080/reserve/get/${user.id}`;
+  console.log(url);
+  console.log(user);
+  axios.get(`http://localhost:8080/reserve/get/${user.id}`)
+      .then(response => {
+        reservations.value = response.data; // 백엔드에서 반환된 예약 목록 저장
+        console.log("정보를 가져왔습니다");
+        console.log(reservations.value);
+      })
+      .catch(error => {
+        console.error('예약 정보를 가져오는 중 에러 발생:', error);
+        console.log(reservations);
+      });
 });
 
 const deleteReservation = (index) => {
   const reservationToDelete = reservations.value[index];
   console.log(reservationToDelete.num); // 예약의 ID 확인
   axios.delete(`http://localhost:8080/reserve/delete/${reservationToDelete.num}`)
-    .then(response =>{
-      reservations.value.splice(index, 1);
-      console.log('예약이 삭제되었습니다.');
-    })
-    .catch(error =>{
-      console.error('예약 삭제 중 에러 발생:', error);
-    })
+      .then(response =>{
+        reservations.value.splice(index, 1);
+        console.log('예약이 삭제되었습니다.');
+      })
+      .catch(error =>{
+        console.error('예약 삭제 중 에러 발생:', error);
+      })
 }
-
-
 
 </script>
 
