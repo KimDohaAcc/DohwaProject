bc6c71f0a1a027a5e989be750f87ca529adeb4a1
KimDohaAcc <8772158@naver.com>
2023-11-21 14:16:12 +0900

    [feat] comment 수정

--- backend/src/main/java/com/ssafy/ssafit/repository/CommentRepository.java
+++ backend/src/main/java/com/ssafy/ssafit/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafit.repository;
 
+import com.ssafy.ssafit.domain.Board;
 import com.ssafy.ssafit.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +8,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-        List<Comment> findAllByBoardNum(Long boardNum);
+        List<Comment> findAllByBoard(Board board);
 
 }
