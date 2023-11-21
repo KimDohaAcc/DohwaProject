a391f54206677fc1ed6bca72e9c93e3af913c974
KimDohaAcc <8772158@naver.com>
2023-11-21 17:58:24 +0900

    [feat] follow 기능 추가 중

--- backend/src/main/java/com/ssafy/ssafit/service/followService/FollowService.java
+++ backend/src/main/java/com/ssafy/ssafit/service/followService/FollowService.java
@@ -4,11 +4,13 @@ import com.ssafy.ssafit.domain.Follow;
 import com.ssafy.ssafit.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FollowService {
     Follow createFollow(Follow follow);
 
     List<Follow> getFollowByFollower(User user);
     List<Follow> getFollowByFollowee(User user);
+    Optional<Follow> getFollowByFollowerAndFollowee(User follower, User followee);
     void removeFollow(Follow follow);
 }
