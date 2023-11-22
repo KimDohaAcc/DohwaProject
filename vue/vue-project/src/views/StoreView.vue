43e47a9ae13be1dbc55cbde761462d4a4f965380
hwasuk528 <hwasuk528@naver.com>
2023-11-22 09:44:52 +0900

    [feat] 카카오맵 구현

--- vue/vue-project/src/views/StoreView.vue
+++ vue/vue-project/src/views/StoreView.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <h2>헬스판다소개합니다.</h2>
+    
     <StoreIntroduce></StoreIntroduce>
+    <div class="container">
+    <h1>카카오 맵</h1>
+    <StoreMap></StoreMap>
+    </div>
   </div>
 </template>
 
 <script setup>
 import StoreIntroduce from '@/components/store/StoreIntroduce.vue';
+import StoreMap from '@/components/store/StoreMap.vue';
 </script>
 
 <style scoped>
 
 </style>
+
+
+
+
